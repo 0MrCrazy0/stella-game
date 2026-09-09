47edf4ffc87ae72b3b83a407a547d729b7566a50
MRCRAZY <102135865+0MrCrazy0@users.noreply.github.com>
2026-09-10 00:00:14 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// --- STELLA'S GAME SERVICE WORKER (v27.0 CREATE 4-PACK) ---
+const CACHE_NAME = 'stellas-game-v27';
+const ASSETS = [
+  './', './index.html', './manifest.json',
+  './icons/icon-192.png', './icons/icon-512.png',
+  './sounds/cat.mp3', './sounds/chicken.mp3', './sounds/cow.mp3',
+  './sounds/dog.mp3', './sounds/duck.mp3', './sounds/goat.mp3',
+  './sounds/pig.mp3', './sounds/rooster.mp3', './sounds/sheep.mp3'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => Promise.all(ASSETS.map(u => c.add(u).catch(() => {})))));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html'))));
+});
